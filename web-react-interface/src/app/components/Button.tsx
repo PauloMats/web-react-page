ae78a359732069fe1cd9adf29af1177cd12268dd
PauloMats <pm.mats98@gmail.com>
2024-12-11 09:40:41 -0300

    feat: initial project structure and repositories

---
+++ web-react-interface/src/app/components/Button.tsx
@@ -0,0 +1,15 @@
+
+
+// const Button = ({ children, onClick, type = "button" }) => {
+//     return (
+//       <button
+//         type={type}
+//         onClick={onClick}
+//         className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+//       >
+//         {children}
+//       </button>
+//     );
+//   };
+  
+//   export default Button;
